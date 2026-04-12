e08bddb9c1ff23dd6e304681dbed234ae09812bc
Aaron Gandžumian <aaron.gandzumian@gmail.com>
2026-04-12 21:55:48 +0300

    Add unit tests to same function file

--- Project_3/aaga_3.py
+++ Project_3/aaga_3.py
@@ -1,6 +1,7 @@
 # 8 uzduotis: patikrinti ar skaičius yra tobulas
 
 import math
+import unittest
 
 def is_perfect_number(n):
    """
@@ -40,11 +41,30 @@ def is_perfect_number(n):
             s += n // i
    return s == n
 
+class Is_Perfect_Number_Test(unittest.TestCase):
+   @classmethod
+   def setUpClass(cls):
+      cls.known_perfect = [6, 28, 496, 8128, 33550336, 8589869056, 137438691328]
+
+   def test_perfect(self):
+      for n in self.known_perfect:
+         self.assertTrue(is_perfect_number(n))
+
+   def test_not_perfect(self):
+      for n in range(-100, 10000):
+         if n not in self.known_perfect:
+            self.assertFalse(is_perfect_number(n))
+
+   def test_invalid_input(self):
+      for n in ['string', True, False, 3.5, -0.0076, [1, 2, 3], (1, 2, 3)]:
+         with self.assertRaises(TypeError):
+            is_perfect_number(n)
+
 def main():
    import doctest
    doctest.testmod(verbose=True)
    return 1
 
-
 if __name__ == '__main__':
-   main()
+   # main()
+   unittest.main(verbosity=2)
