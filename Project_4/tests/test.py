b6738d48139e3fd01219f61bb132da9e542e8ca9
Aaron Gandžumian <aaron.gandzumian@gmail.com>
2026-04-17 14:58:29 +0300

    Implement unit tests for rational type

--- Project_4/tests/test.py
+++ Project_4/tests/test.py
@@ -2,7 +2,7 @@ import unittest
 from mypackage import rational
 from mypackage import perfect
 
-class Perfect_Number_Test(unittest.TestCase):
+class Perfect_Test(unittest.TestCase):
    @classmethod
    def setUpClass(cls):
       cls.known_perfect = [6, 28, 496, 8128, 33550336, 8589869056, 137438691328]
@@ -20,6 +20,29 @@ class Perfect_Number_Test(unittest.TestCase):
       for n in ['string', 3.5, -0.0076, [1, 2, 3], (1, 2, 3)]:
          with self.assertRaises(TypeError):
             perfect(n)
+
+class Rational_Test(unittest.TestCase):
+   def test_rational_repr(self):
+      a = rational(1, 2)
+      assert repr(a) == "Rational(1, 2)"
+      a = rational(13, 39)
+      assert repr(a) == "Rational(1, 3)"
+
+   def test_rational_str(self):
+      a = rational(5, 7)
+      assert str(a) == "5/7"
+      a = rational(8, 6)
+      assert str(a) == "4/3"
+
+   def test_rational_add(self):
+      a = rational(2, 7)
+      b = rational(3, 7)
+      c = a.add(b)
+      assert repr(c) == "Rational(5, 7)"
+      a = rational(5, 6)
+      b = rational(3, 14)
+      c = a.add(b)
+      assert repr(c) == "Rational(22, 21)"
 
 if __name__ == '__main__':
    unittest.main(verbosity=2)
