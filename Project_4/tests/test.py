52d737a14339c50489f540f951b02edfc3c5759d
Aaron Gandžumian <aaron.gandzumian@gmail.com>
2026-04-17 14:37:01 +0300

    Implement tests for perfect module

---
+++ Project_4/tests/test.py
@@ -0,0 +1,25 @@
+import unittest
+from mypackage import rational
+from mypackage import perfect
+
+class Perfect_Number_Test(unittest.TestCase):
+   @classmethod
+   def setUpClass(cls):
+      cls.known_perfect = [6, 28, 496, 8128, 33550336, 8589869056, 137438691328]
+
+   def test_perfect(self):
+      for n in self.known_perfect:
+         self.assertTrue(perfect(n))
+
+   def test_not_perfect(self):
+      for n in range(-100, 10000):
+         if n not in self.known_perfect:
+            self.assertFalse(perfect(n))
+
+   def test_invalid_input(self):
+      for n in ['string', 3.5, -0.0076, [1, 2, 3], (1, 2, 3)]:
+         with self.assertRaises(TypeError):
+            perfect(n)
+
+if __name__ == '__main__':
+   unittest.main(verbosity=2)
