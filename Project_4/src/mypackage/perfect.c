1c5e3a9402dcc324fa5b20c6a953b2b53dfbcd02
Aaron Gandžumian <aaron.gandzumian@gmail.com>
2026-04-17 03:32:52 +0300

    Add error object and update description

--- Project_4/src/mypackage/perfect.c
+++ Project_4/src/mypackage/perfect.c
@@ -7,7 +7,10 @@ static PyObject *is_perfect(PyObject *self, PyObject *args)
    long n;
 
    if (!PyArg_ParseTuple(args, "l", &n))
+   {
+      PyErr_SetString(PyExc_TypeError, "Expected an integer argument");
       return NULL;
+   }
 
    if (n < 6)
       return PyBool_FromLong(0);
@@ -29,7 +32,9 @@ static PyObject *is_perfect(PyObject *self, PyObject *args)
 }
 
 static PyMethodDef MyMethods[] = {
-    {"perfect", is_perfect, METH_VARARGS, "Check if a number is perfect."},
+    {"perfect", is_perfect, METH_VARARGS,
+     "perfect(n) -> bool\n\n"
+     "Return True if n is a perfect number (e.g. 6, 28, 496)."},
     {NULL, NULL, 0, NULL}};
 
 static struct PyModuleDef mymodule = {
